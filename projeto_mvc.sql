ffeed3326d8b1240925589abc1086080dba22a1b
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2025-01-08 20:42:05 -0300

    sql: add sql schema of project - `projeto_mvc.sql`

---
+++ projeto_mvc.sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 09/01/2025 às 00:39
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `projeto_mvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `depoimentos`
+--
+
+CREATE TABLE `depoimentos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `mensagem` text NOT NULL,
+  `data` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `depoimentos`
+--
+
+INSERT INTO `depoimentos` (`id`, `nome`, `mensagem`, `data`) VALUES
+(1, 'Alefe ', 'Lorem ipsum dolor sit ame', '2025-01-06 22:50:12'),
+(2, 'Canal Wdev', 'Lorem ipsum dolor sit ameLorem ipsum dolor sit ameLorem ipsum dolor sit ameLorem ipsum dolor sit ame. ANAL', '2025-01-06 23:11:54'),
+(3, 'Anonimo', 'andar é otimo!', '2025-01-06 23:32:19'),
+(4, 'Bicicleta', 'Teste de depoimentos', '2025-01-06 23:32:40'),
+(5, 'mas98Sw_', 'biribinha', '2025-01-06 23:32:55'),
+(6, 'max ual', 'sou ual', '2025-01-06 23:35:09');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices de tabela `depoimentos`
+--
+ALTER TABLE `depoimentos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `depoimentos`
+--
+ALTER TABLE `depoimentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
